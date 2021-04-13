69c039f1fa3aedeeb42b2b5d0802f216820d67f9
Alexander Nikolas Reuber <info@unconnect.net>
2021-04-14 00:09:32 +0200

    integrated own bootstrap theme

--- packages/nikolas-real-frontend/src/components/index.js
+++ packages/nikolas-real-frontend/src/components/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import {connect, Global, Head, css, styled} from "frontity"
-import Link from "@frontity/components/link"
 import Switch from "@frontity/components/switch"
 
 import {Container, Row, Col, Card, Jumbotron} from 'react-bootstrap';
@@ -20,23 +19,22 @@ const Root = ({state}) => {
     const data = state.source.get(state.router.link)
 
     // Get current Date and set the year for use in footer
-    var currentDate = new Date()
-    var thisYear = currentDate.getFullYear()
-    var theYears = (2021 == thisYear) ? thisYear : '2021 - ' + thisYear
+    let currentDate = new Date()
+    let thisYear = currentDate.getFullYear()
+    let theYears = (2021 == thisYear) ? thisYear : '2021 - ' + thisYear
 
     // Get Brandname from frontity theme state for use in Navbar
     const brandname = state.theme.brandname
 
+    // Set styles path
+    let bootstrapStyles = "https://nikolas-frontend-styles.vercel.app/css/nikolas-styles.css"
     return (
         <>
            <Title />
            <Head>
-               // TODO: Include with defer or async or maybe totally different. Do more research!
                 <link
                     rel="stylesheet"
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-                    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
-                    crossOrigin="anonymous"
+                    href={bootstrapStyles}
                 />
             </Head>
 
@@ -78,22 +76,6 @@ const Root = ({state}) => {
 export default connect(Root)
 
 const globalStyle = css`
-                  // TODO: this should be done with fontloader, 
-                  // see https://github.com/frontity-demos/frontity-examples/tree/master/demo-using-google-fonts
-                  @import url('https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,300;0,400;0,600;0,900;1,300;1,400;1,600;1,900&family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap');
-
-                  html {
-                    font-family: "Fira Sans", system-ui, Arial, sans-serif;
-                  }
-
-                  h1, h2, h3, h4, h5, h6 {
-                    font-family: "Merriweather";
-                  }
-
-                  p, a, ol, ul, a {
-
-                  }
-      
                   img {
                     max-width: 100%;
                   }
